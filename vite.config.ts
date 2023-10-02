33d9a6f0d97697aff03b697e24682571ef94e755
Antoine Cheminat <ac.dev@hotmail.com>
2023-10-02 05:30:48 -0400

    Install postcss plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import postcssNested from 'postcss-nested';
+import postcssCustomMedia from 'postcss-custom-media';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    postcss: {
+      plugins: [postcssCustomMedia(), postcssNested()],
+    },
+  },
+});
