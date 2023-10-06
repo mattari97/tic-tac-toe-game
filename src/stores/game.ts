d184efa164307c4f1a0b124affa394e3deda02c6
Antoine Cheminat <ac.dev@hotmail.com>
2023-10-06 11:21:30 -0400

    Make the bot smart

--- src/stores/game.ts
+++ src/stores/game.ts
@@ -1,90 +1,7 @@
 import { create } from 'zustand';
-import { Board, Gamemode, Mark, Player, Store, Result } from '../types';
-
-const DEFAULT_PLAYER: Player = { name: 'P1', score: 0 };
-const DEFAULT_BOARD: Board = ['', '', '', '', '', '', '', '', ''];
-const WIN_COMBINATIONS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [6, 4, 2],
-];
-
-const checkWinner = (
-  currentBoard: Store['currentBoard'],
-  currentMark: Store['currentMark'],
-  nbOfMoves: number
-): boolean => {
-  if (nbOfMoves < 5) return false;
-  for (let i = 0; i < WIN_COMBINATIONS.length; i++) {
-    const curr = WIN_COMBINATIONS[i];
-    if (
-      currentBoard[curr[0]] === currentMark &&
-      currentBoard[curr[1]] === currentMark &&
-      currentBoard[curr[2]] === currentMark
-    ) {
-      return true;
-    }
-  }
-  return false;
-};
-
-const checkDraw = (currentBoard: Store['currentBoard'], nbOfMoves: number) => {
-  if (nbOfMoves < 7) return false;
-  for (let i = 0; i < currentBoard.length; i++) {
-    const cell = currentBoard[i];
-    if (cell === '') break;
-    if (i === 8) return true;
-  }
-  return false;
-};
-
-const updateWinningScores = (store: Store): Pick<Store, 'playerO' | 'playerX'> => {
-  if (store.currentMark === 'x') {
-    const playerX: Player = { ...store.playerX, score: ++store.playerX.score };
-    return { playerX, playerO: store.playerO };
-  } else {
-    const playerO: Player = { ...store.playerO, score: ++store.playerO.score };
-    return { playerO, playerX: store.playerX };
-  }
-};
-
-const getWinningMessage = (store: Store): Result => {
-  const winningPlayer = store.currentMark === 'x' ? store.playerX : store.playerO;
-  let message: string;
-  switch (winningPlayer.name) {
-    case 'Cpu':
-      message = 'Oh no, you lost…';
-      break;
-    case 'You':
-      message = 'You won!';
-      break;
-    case 'P1':
-      message = 'Player 1 wins!';
-      break;
-    case 'P2':
-      message = 'Player 2 wins!';
-  }
-  return { type: store.currentMark, message };
-};
-
-const getCpuNextMove = (currentBoard: Store['currentBoard']) => {
-  const clone = [...currentBoard];
-  const legalMoves: number[] = [];
-  for (let i = 0; i < clone.length; i++) {
-    const curr = clone[i];
-    if (curr !== '') continue;
-    legalMoves.push(i);
-  }
-  const randomIndex = legalMoves[Math.floor(Math.random() * legalMoves.length)];
-  return randomIndex;
-};
-
-// const sleep = (delay: number) => new Promise((resolve) => setTimeout(resolve, delay));
+import { Gamemode, Mark, Player, Store, Result } from '../types';
+import { DEFAULT_PLAYER, DEFAULT_BOARD } from './constants';
+import { checkDraw, checkWinner, getCpuNextMove, getWinningMessage, updateWinningScores } from './helpers';
 
 /* ----------------- */
 /* Game Store        */
@@ -120,7 +37,7 @@ const useStore = create<Store>()((set) => ({
 
       if (state.isCpuMove) {
         console.log('cpu move');
-        const cpuMoveIndex = getCpuNextMove(state.currentBoard);
+        const cpuMoveIndex = getCpuNextMove(state.currentBoard, state.currentMark);
         currentBoard = state.currentBoard.map((value, i) => (i === cpuMoveIndex ? state.currentMark : value));
       } else {
         console.log('player move');
@@ -139,7 +56,6 @@ const useStore = create<Store>()((set) => ({
       } else {
         const currentMark: Mark = state.currentMark === 'x' ? 'o' : 'x';
         const isCpuMove = !state.isCpuMove && state.gamemode === 'cpu' ? true : false;
-        console.log('store', isCpuMove);
         return { ...state, currentBoard, currentMark, nbOfMoves, isCpuMove };
       }
     }),
